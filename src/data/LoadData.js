4e3de575d9f3fb8dd42159c7e6e6042bc111e26c
Andrew Ringler <aringler@payette.com>
2020-02-13 11:58:31 -0500

    minor cleanup

--- src/data/LoadData.js
+++ src/data/LoadData.js
@@ -2,22 +2,26 @@ import Papa from 'papaparse';
 import dataGWP from './gwp.csv';
 import dataAllImpacts from './allimpacts.csv';
 
+const PAPAPARSE_CONFIG = {
+  download: true,
+  header: true,
+  skipEmptyLines: true
+};
+
 export default {
   allImpactsData: (cb) => {
     Papa.parse(dataAllImpacts, {
-          download: true,
-          header: true,
-          skipEmptyLines: true,
-          complete: function(results, file) {
-           if(Array.isArray(results.data)) {
-             const allImpactsData = results.data.map(d => {
-               return { letter: d.Variable, frequency: parseFloat(d.Attended) }
-             });
-             cb(allImpactsData);
-           } else {
-             console.error('error trying to load file', results.errors);
-           }
-         }
+      ...PAPAPARSE_CONFIG,
+      complete: function(results, file) {
+       if(Array.isArray(results.data)) {
+         const allImpactsData = results.data.map(d => {
+           return { letter: d.Variable, frequency: parseFloat(d.Attended) }
+         });
+         cb(allImpactsData);
+       } else {
+         console.error('error trying to load file', results.errors);
+       }
+     }
     });
   }
 };
