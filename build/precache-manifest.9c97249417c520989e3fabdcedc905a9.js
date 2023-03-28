54d429dc4d6ef6855279045488efedf4f8678355
yushiw219 <ywang@payette.com>
2023-03-28 10:45:46 -0400

    default selection changed

---
+++ build/precache-manifest.9c97249417c520989e3fabdcedc905a9.js
@@ -0,0 +1,466 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ba7e12a7ca5418e87a13aa2066adc3",
+    "url": "/Kaleidoscope/index.html"
+  },
+  {
+    "revision": "ea79beb997ae7876d8c3",
+    "url": "/Kaleidoscope/static/css/2.119f37bc.chunk.css"
+  },
+  {
+    "revision": "493bd3d7737db436a68c",
+    "url": "/Kaleidoscope/static/css/main.b786716a.chunk.css"
+  },
+  {
+    "revision": "ea79beb997ae7876d8c3",
+    "url": "/Kaleidoscope/static/js/2.305db699.chunk.js"
+  },
+  {
+    "revision": "43b5e7443d98e38f1598dea4380e0bb8",
+    "url": "/Kaleidoscope/static/js/2.305db699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493bd3d7737db436a68c",
+    "url": "/Kaleidoscope/static/js/main.db718b74.chunk.js"
+  },
+  {
+    "revision": "eb66c269c395d9e31709",
+    "url": "/Kaleidoscope/static/js/runtime-main.6c3a72c8.js"
+  },
+  {
+    "revision": "051d7a41d0f0e3138d55652433d90e1a",
+    "url": "/Kaleidoscope/static/media/Detail_MockUp_MVGranite.051d7a41.png"
+  },
+  {
+    "revision": "c4fec62243b7eec778296e24418cfc27",
+    "url": "/Kaleidoscope/static/media/MaterialBreakdown-11.c4fec622.png"
+  },
+  {
+    "revision": "d58661bf13fe3d08d97345aa3f5cc8a8",
+    "url": "/Kaleidoscope/static/media/System_Boundary-flooring_EDIT.d58661bf.png"
+  },
+  {
+    "revision": "7339ce93c52531b29cf4a1bcbdc6dbc2",
+    "url": "/Kaleidoscope/static/media/System_Boundary-partitions.7339ce93.png"
+  },
+  {
+    "revision": "5b94e3d01cd7d9f81c3a683714e448b2",
+    "url": "/Kaleidoscope/static/media/allImpact_10y_n-1.5b94e3d0.csv"
+  },
+  {
+    "revision": "1754212c21a0da0d825204e3752aa054",
+    "url": "/Kaleidoscope/static/media/allImpact_10y_y-1.1754212c.csv"
+  },
+  {
+    "revision": "3eadf22e3ca18803031369058549d61f",
+    "url": "/Kaleidoscope/static/media/allImpact_60y_n-1.3eadf22e.csv"
+  },
+  {
+    "revision": "a60456fd747ef65bf3e5984545259125",
+    "url": "/Kaleidoscope/static/media/allImpact_60y_y-1.a60456fd.csv"
+  },
+  {
+    "revision": "f99762bef13222824e7fd291f28e93ed",
+    "url": "/Kaleidoscope/static/media/allImpact_60yd_n-1.f99762be.csv"
+  },
+  {
+    "revision": "b9139ce56c3f3b9281a1cf1ded287157",
+    "url": "/Kaleidoscope/static/media/allImpact_60yd_y-1.b9139ce5.csv"
+  },
+  {
+    "revision": "c754d2d6483ebcff37483ee33ce9713b",
+    "url": "/Kaleidoscope/static/media/ceilings_allImpact_10y_n.c754d2d6.csv"
+  },
+  {
+    "revision": "0ba7501f98b6f59e3bfe773c7eaa88e9",
+    "url": "/Kaleidoscope/static/media/ceilings_allImpact_10y_y.0ba7501f.csv"
+  },
+  {
+    "revision": "917dd3db47de95a8572730e81d5bc988",
+    "url": "/Kaleidoscope/static/media/ceilings_allImpact_60y_n.917dd3db.csv"
+  },
+  {
+    "revision": "26ffb3269e40b05bf9ff7a6ff3393108",
+    "url": "/Kaleidoscope/static/media/ceilings_allImpact_60y_y.26ffb326.csv"
+  },
+  {
+    "revision": "a7823647c592b5822fa522baced93bce",
+    "url": "/Kaleidoscope/static/media/ceilings_allImpact_60yd_n.a7823647.csv"
+  },
+  {
+    "revision": "99360d7e59342a253886f68351c7cc8b",
+    "url": "/Kaleidoscope/static/media/ceilings_allImpact_60yd_y.99360d7e.csv"
+  },
+  {
+    "revision": "83c7c6b87ac759d55af75651a98a295d",
+    "url": "/Kaleidoscope/static/media/ceilings_gwp_10y_n.83c7c6b8.csv"
+  },
+  {
+    "revision": "a34fa97b41b8549ac790c512a67460a5",
+    "url": "/Kaleidoscope/static/media/ceilings_gwp_10y_y.a34fa97b.csv"
+  },
+  {
+    "revision": "e9ca70e0bb3f21e009cf729ab2953d99",
+    "url": "/Kaleidoscope/static/media/ceilings_gwp_60y_n.e9ca70e0.csv"
+  },
+  {
+    "revision": "387c591a6e036cb103940d6e0eb50809",
+    "url": "/Kaleidoscope/static/media/ceilings_gwp_60y_y.387c591a.csv"
+  },
+  {
+    "revision": "8694df086df845af710dece99cab4aaf",
+    "url": "/Kaleidoscope/static/media/ceilings_gwp_60yd_n.8694df08.csv"
+  },
+  {
+    "revision": "adeb0efe80fbbbb6d9859c8b7d51fd79",
+    "url": "/Kaleidoscope/static/media/ceilings_gwp_60yd_y.adeb0efe.csv"
+  },
+  {
+    "revision": "f3d546b52b80a5445f4437d72b7ba3c9",
+    "url": "/Kaleidoscope/static/media/ceilings_lcs_10y_n.f3d546b5.csv"
+  },
+  {
+    "revision": "8a5d01f86c5b9039d8bb9589ec511368",
+    "url": "/Kaleidoscope/static/media/ceilings_lcs_10y_y.8a5d01f8.csv"
+  },
+  {
+    "revision": "cca7017d07116d76b89c35f42648dea5",
+    "url": "/Kaleidoscope/static/media/ceilings_lcs_60y_n.cca7017d.csv"
+  },
+  {
+    "revision": "708f1c7740057da45da7f3e242e1f1d4",
+    "url": "/Kaleidoscope/static/media/ceilings_lcs_60y_y.708f1c77.csv"
+  },
+  {
+    "revision": "8e4b04e0886d1ae16266f6e600024b79",
+    "url": "/Kaleidoscope/static/media/ceilings_lcs_60yd_n.8e4b04e0.csv"
+  },
+  {
+    "revision": "0a1d372194258373ed7483a31af750b8",
+    "url": "/Kaleidoscope/static/media/ceilings_lcs_60yd_y.0a1d3721.csv"
+  },
+  {
+    "revision": "9a7d6a748e9b2059a38058a8d302a8ae",
+    "url": "/Kaleidoscope/static/media/ceilings_material_10y_n.9a7d6a74.csv"
+  },
+  {
+    "revision": "5c1555da55de1f06e6c5b485c4924ffe",
+    "url": "/Kaleidoscope/static/media/ceilings_material_10y_y.5c1555da.csv"
+  },
+  {
+    "revision": "944bf07759ceaeca812cba21ff003015",
+    "url": "/Kaleidoscope/static/media/ceilings_material_60y_n.944bf077.csv"
+  },
+  {
+    "revision": "c350717c9f923f3aa30a229065139fce",
+    "url": "/Kaleidoscope/static/media/ceilings_material_60y_y.c350717c.csv"
+  },
+  {
+    "revision": "8c99d207db52d69e48926d382a7ce559",
+    "url": "/Kaleidoscope/static/media/ceilings_material_60yd_n.8c99d207.csv"
+  },
+  {
+    "revision": "eabc79b12dc9dacf4087e1be6fd13723",
+    "url": "/Kaleidoscope/static/media/ceilings_material_60yd_y.eabc79b1.csv"
+  },
+  {
+    "revision": "7548d9eb64be977c8b6f277de86a3215",
+    "url": "/Kaleidoscope/static/media/ceilingslegendaxon.7548d9eb.png"
+  },
+  {
+    "revision": "341be67df246d50888486a80e0b6bce1",
+    "url": "/Kaleidoscope/static/media/flooring_allImpact_10y_n.341be67d.csv"
+  },
+  {
+    "revision": "51851373c4e66698c9ad8f0bcb183f72",
+    "url": "/Kaleidoscope/static/media/flooring_allImpact_10y_y.51851373.csv"
+  },
+  {
+    "revision": "affb8bee03d4e49acec1b6cd62fc46cf",
+    "url": "/Kaleidoscope/static/media/flooring_allImpact_60y_n.affb8bee.csv"
+  },
+  {
+    "revision": "57efbace784748d53b51cb8de5eeccf6",
+    "url": "/Kaleidoscope/static/media/flooring_allImpact_60y_y.57efbace.csv"
+  },
+  {
+    "revision": "364e693ef677df646aadbd2df983242a",
+    "url": "/Kaleidoscope/static/media/flooring_allImpact_60yd_n.364e693e.csv"
+  },
+  {
+    "revision": "a71aaf9b4ad17f64a276ff86398a4a95",
+    "url": "/Kaleidoscope/static/media/flooring_allImpact_60yd_y.a71aaf9b.csv"
+  },
+  {
+    "revision": "4bb440dda58bee8a9bb15d5698d9b7c2",
+    "url": "/Kaleidoscope/static/media/flooring_gwp_10y_n.4bb440dd.csv"
+  },
+  {
+    "revision": "6d9fff93f10c38043598e140b7414901",
+    "url": "/Kaleidoscope/static/media/flooring_gwp_10y_y.6d9fff93.csv"
+  },
+  {
+    "revision": "08a84bd15eb0a5f6bee0e7df75d40d92",
+    "url": "/Kaleidoscope/static/media/flooring_gwp_60y_n.08a84bd1.csv"
+  },
+  {
+    "revision": "b371b82cfdc2d2b9c110cb94cce51840",
+    "url": "/Kaleidoscope/static/media/flooring_gwp_60y_y.b371b82c.csv"
+  },
+  {
+    "revision": "1e1d37e7ac7c77aa87878619865bb0d4",
+    "url": "/Kaleidoscope/static/media/flooring_gwp_60yd_n.1e1d37e7.csv"
+  },
+  {
+    "revision": "1bdba14341a39dfc62551c32c4124f41",
+    "url": "/Kaleidoscope/static/media/flooring_gwp_60yd_y.1bdba143.csv"
+  },
+  {
+    "revision": "62825591b4db52d583b030410c4917d7",
+    "url": "/Kaleidoscope/static/media/flooring_lcs_10y_n.62825591.csv"
+  },
+  {
+    "revision": "930d95786b531ac86def88e7db5d3547",
+    "url": "/Kaleidoscope/static/media/flooring_lcs_10y_y.930d9578.csv"
+  },
+  {
+    "revision": "30de8ab32293d9500bc6f7dc1705ff40",
+    "url": "/Kaleidoscope/static/media/flooring_lcs_60y_n.30de8ab3.csv"
+  },
+  {
+    "revision": "9af9a9130ba3d76af713d37c52361f50",
+    "url": "/Kaleidoscope/static/media/flooring_lcs_60y_y.9af9a913.csv"
+  },
+  {
+    "revision": "46b8205235f378bb3e26e438f9085cc5",
+    "url": "/Kaleidoscope/static/media/flooring_lcs_60yd_n.46b82052.csv"
+  },
+  {
+    "revision": "4e737e4b8f8d6c828b2d6670300f57d3",
+    "url": "/Kaleidoscope/static/media/flooring_lcs_60yd_y.4e737e4b.csv"
+  },
+  {
+    "revision": "507aa5d23043a27229fb78193a6befc7",
+    "url": "/Kaleidoscope/static/media/flooring_material_10y_n.507aa5d2.csv"
+  },
+  {
+    "revision": "51f858ee60635dceaaf6294c3c1328a8",
+    "url": "/Kaleidoscope/static/media/flooring_material_10y_y.51f858ee.csv"
+  },
+  {
+    "revision": "2356186c7220103ca3004658659baec0",
+    "url": "/Kaleidoscope/static/media/flooring_material_60y_n.2356186c.csv"
+  },
+  {
+    "revision": "5382840cc57f7ccbcf3652af498ebb44",
+    "url": "/Kaleidoscope/static/media/flooring_material_60y_y.5382840c.csv"
+  },
+  {
+    "revision": "b84499d7babb7cdab514ef1bb881e6f0",
+    "url": "/Kaleidoscope/static/media/flooring_material_60yd_n.b84499d7.csv"
+  },
+  {
+    "revision": "204a47f1b10c2d6d54506162a14af318",
+    "url": "/Kaleidoscope/static/media/flooring_material_60yd_y.204a47f1.csv"
+  },
+  {
+    "revision": "1b89bd550c6d97c31dec05172c7f1d80",
+    "url": "/Kaleidoscope/static/media/font.1b89bd55.woff"
+  },
+  {
+    "revision": "50ba1fbc3cd2758a4645e63a54539c30",
+    "url": "/Kaleidoscope/static/media/font.50ba1fbc.woff2"
+  },
+  {
+    "revision": "8b5eb67a68d729ef9373433c0fe74413",
+    "url": "/Kaleidoscope/static/media/font.8b5eb67a.woff"
+  },
+  {
+    "revision": "8be51b1272b11cfaf69c1e758d6a989d",
+    "url": "/Kaleidoscope/static/media/font.8be51b12.woff2"
+  },
+  {
+    "revision": "c39db432b42161ff4cb8b9f151a3797b",
+    "url": "/Kaleidoscope/static/media/font.c39db432.woff2"
+  },
+  {
+    "revision": "c63c2fe1d0e5a8ae8134eba1c83990f0",
+    "url": "/Kaleidoscope/static/media/font.c63c2fe1.woff2"
+  },
+  {
+    "revision": "db863eedb8d980f462dd021f78a3051b",
+    "url": "/Kaleidoscope/static/media/font.db863eed.woff"
+  },
+  {
+    "revision": "f9746871dc49b75b6f5b18386146d58a",
+    "url": "/Kaleidoscope/static/media/font.f9746871.woff"
+  },
+  {
+    "revision": "0ad1c3aa8edc01def2ff6512ff040ba1",
+    "url": "/Kaleidoscope/static/media/gwp_10y_n-1.0ad1c3aa.csv"
+  },
+  {
+    "revision": "b392783c208cf53c6d70784c7fb85f59",
+    "url": "/Kaleidoscope/static/media/gwp_10y_y-1.b392783c.csv"
+  },
+  {
+    "revision": "9058a0af40b5c15930be74e8a853b240",
+    "url": "/Kaleidoscope/static/media/gwp_60y_n-1.9058a0af.csv"
+  },
+  {
+    "revision": "5a12e6741abd48fbbbd5cc3deaa8e4e9",
+    "url": "/Kaleidoscope/static/media/gwp_60y_y-1.5a12e674.csv"
+  },
+  {
+    "revision": "326ce151d4d3f0d8add87579bb7bea0d",
+    "url": "/Kaleidoscope/static/media/gwp_60yd_n-1.326ce151.csv"
+  },
+  {
+    "revision": "0fb0365829b3872ef00c390a29f4d21b",
+    "url": "/Kaleidoscope/static/media/gwp_60yd_y-1.0fb03658.csv"
+  },
+  {
+    "revision": "5449d6b96a7f51b6758f371699760c2f",
+    "url": "/Kaleidoscope/static/media/lcs_10y_n-1.5449d6b9.csv"
+  },
+  {
+    "revision": "a5539b5fde4bb4888ba6718f474b58e3",
+    "url": "/Kaleidoscope/static/media/lcs_10y_y-1.a5539b5f.csv"
+  },
+  {
+    "revision": "249fa5e4d4e34348e7609178d6d66382",
+    "url": "/Kaleidoscope/static/media/lcs_60y_n-1.249fa5e4.csv"
+  },
+  {
+    "revision": "1298a07c1a6d3837779358c3b511ded3",
+    "url": "/Kaleidoscope/static/media/lcs_60y_y-1.1298a07c.csv"
+  },
+  {
+    "revision": "307846911f6a4f8275b12c3aaec5c229",
+    "url": "/Kaleidoscope/static/media/lcs_60yd_n-1.30784691.csv"
+  },
+  {
+    "revision": "dc481c4aab328dce9155ab486f935b5a",
+    "url": "/Kaleidoscope/static/media/lcs_60yd_y-1.dc481c4a.csv"
+  },
+  {
+    "revision": "c8523c497b71fc3cdb99d1f99afae5cf",
+    "url": "/Kaleidoscope/static/media/material_10y_n-1.c8523c49.csv"
+  },
+  {
+    "revision": "f1ced4e07fcad3fb415f581eda59c064",
+    "url": "/Kaleidoscope/static/media/material_10y_y-1.f1ced4e0.csv"
+  },
+  {
+    "revision": "54c779d91e145ff6f86b3c00e945a472",
+    "url": "/Kaleidoscope/static/media/material_60y_n-1.54c779d9.csv"
+  },
+  {
+    "revision": "018e44457bdf5a8f929fa7fa77f9fb04",
+    "url": "/Kaleidoscope/static/media/material_60y_y-1.018e4445.csv"
+  },
+  {
+    "revision": "c34ca38d05a5199d938a84534c8f5409",
+    "url": "/Kaleidoscope/static/media/material_60yd_n-1.c34ca38d.csv"
+  },
+  {
+    "revision": "e9088c0be9a6e0f9dc2a97befe5b9925",
+    "url": "/Kaleidoscope/static/media/material_60yd_y-1.e9088c0b.csv"
+  },
+  {
+    "revision": "73ef3656b7343865a07c34107161f4ce",
+    "url": "/Kaleidoscope/static/media/partitions_allImpact_10y_n.73ef3656.csv"
+  },
+  {
+    "revision": "a6cfab368b76ad756d8e261a28217c98",
+    "url": "/Kaleidoscope/static/media/partitions_allImpact_10y_y.a6cfab36.csv"
+  },
+  {
+    "revision": "377f62cffc4f9e27fe93b70838fe7d3b",
+    "url": "/Kaleidoscope/static/media/partitions_allImpact_60y_n.377f62cf.csv"
+  },
+  {
+    "revision": "5c6bafbac9a31e677f86698ecd94b47e",
+    "url": "/Kaleidoscope/static/media/partitions_allImpact_60y_y.5c6bafba.csv"
+  },
+  {
+    "revision": "f63d01fe4a8fe0fd2f96c6dcd3aa2dc9",
+    "url": "/Kaleidoscope/static/media/partitions_allImpact_60yd_n.f63d01fe.csv"
+  },
+  {
+    "revision": "e8e881d6e5ce9640d6bb5001f130ae8d",
+    "url": "/Kaleidoscope/static/media/partitions_allImpact_60yd_y.e8e881d6.csv"
+  },
+  {
+    "revision": "6078926c9de6abaee0c25b4dd7d5b7e0",
+    "url": "/Kaleidoscope/static/media/partitions_gwp_10y_n.6078926c.csv"
+  },
+  {
+    "revision": "b2a3aa25c0350e2f71268934b1bc60da",
+    "url": "/Kaleidoscope/static/media/partitions_gwp_10y_y.b2a3aa25.csv"
+  },
+  {
+    "revision": "b0e43e83726aa2c5654a9226641644de",
+    "url": "/Kaleidoscope/static/media/partitions_gwp_60y_n.b0e43e83.csv"
+  },
+  {
+    "revision": "5aaea4dc1f66f527820abde5d80ef28c",
+    "url": "/Kaleidoscope/static/media/partitions_gwp_60y_y.5aaea4dc.csv"
+  },
+  {
+    "revision": "e48226f03d10b2389a00948d2045c05e",
+    "url": "/Kaleidoscope/static/media/partitions_gwp_60yd_n.e48226f0.csv"
+  },
+  {
+    "revision": "286b6123eead6154525010018ddfd5ba",
+    "url": "/Kaleidoscope/static/media/partitions_gwp_60yd_y.286b6123.csv"
+  },
+  {
+    "revision": "73d518bebc7912f88068dd3f70913174",
+    "url": "/Kaleidoscope/static/media/partitions_lcs_10y_n.73d518be.csv"
+  },
+  {
+    "revision": "2d4955eb96b70184d92e862f8c5e66c3",
+    "url": "/Kaleidoscope/static/media/partitions_lcs_10y_y.2d4955eb.csv"
+  },
+  {
+    "revision": "b3266f2d794080c301719c2facd3c242",
+    "url": "/Kaleidoscope/static/media/partitions_lcs_60y_n.b3266f2d.csv"
+  },
+  {
+    "revision": "04ad12b52cfa56681e99f171620ba3e4",
+    "url": "/Kaleidoscope/static/media/partitions_lcs_60y_y.04ad12b5.csv"
+  },
+  {
+    "revision": "0f90b0477d76cda4a0e0718d96be34a4",
+    "url": "/Kaleidoscope/static/media/partitions_lcs_60yd_n.0f90b047.csv"
+  },
+  {
+    "revision": "63a5c8a8bafae078617837db1cb73c40",
+    "url": "/Kaleidoscope/static/media/partitions_lcs_60yd_y.63a5c8a8.csv"
+  },
+  {
+    "revision": "ac56d23f6fc49d7e84f23c750212a201",
+    "url": "/Kaleidoscope/static/media/partitions_material_10y_n.ac56d23f.csv"
+  },
+  {
+    "revision": "ebb87a755cf3cccb3488f86839b08f35",
+    "url": "/Kaleidoscope/static/media/partitions_material_10y_y.ebb87a75.csv"
+  },
+  {
+    "revision": "4ec0358ea6040827897ac548bd5ce146",
+    "url": "/Kaleidoscope/static/media/partitions_material_60y_n.4ec0358e.csv"
+  },
+  {
+    "revision": "3c58045f2be752afb2611044073f1f9f",
+    "url": "/Kaleidoscope/static/media/partitions_material_60y_y.3c58045f.csv"
+  },
+  {
+    "revision": "ad20dd766d45814eb6d965faec2952b1",
+    "url": "/Kaleidoscope/static/media/partitions_material_60yd_n.ad20dd76.csv"
+  },
+  {
+    "revision": "cbd61db66bc04edc7c6707f9010df28e",
+    "url": "/Kaleidoscope/static/media/partitions_material_60yd_y.cbd61db6.csv"
+  }
+]);
